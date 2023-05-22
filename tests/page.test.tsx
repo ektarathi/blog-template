8452edab37934e4a22a902ed381cfc2e9bc884df
Ekta Rathi <er@arenko.group>
2023-05-22 08:51:47 +0100

    Updated tests

--- tests/page.test.tsx
+++ tests/page.test.tsx
@@ -1,27 +1,48 @@
-import * as React from 'react';
-import { render, screen } from '@testing-library/react';
-import Page from '../src/app/page'
+import * as React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Page from "../src/app/page";
 
-import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
 import lightThemeOptions from "../styles/theme/lightThemeOption";
 
 describe("Page", () => {
-    const lightTheme = createTheme(lightThemeOptions);
-    
-    it("renders main heading", async () => {
-      render(
-        <ThemeProvider theme={lightTheme}>
-            <Page />
-        </ThemeProvider>
-        
-      );
-  
-      await screen.findByTestId("main-section");
-      expect(screen.getByTestId("main-section")).toBeInTheDocument();
-      //expect(screen.getByTestId("main-section")).toHaveBeenCalledWith("Travel With Us");      
+  const lightTheme = createTheme(lightThemeOptions);
 
-      await screen.findByTestId("main-heading");
-      expect(screen.getByTestId("main-heading")).toBeInTheDocument();
-      expect(screen.getByTestId("main-heading")).toHaveTextContent("Travel With Us");      
-    });
+  it("renders the page", async () => {
+    render(
+      <ThemeProvider theme={lightTheme}>
+        <Page />
+      </ThemeProvider>
+    );
+
+    await screen.findByTestId("main-section");
+    expect(screen.getByTestId("main-section")).toBeInTheDocument();
+
+    await screen.findByTestId("main-heading");
+    expect(screen.getByTestId("main-heading")).toBeInTheDocument();
+    expect(screen.getByTestId("main-heading")).toHaveTextContent(
+      "Travel With Us"
+    );
+
+    await screen.findByTestId("sub-heading");
+    expect(screen.getByTestId("sub-heading")).toBeInTheDocument();
+    expect(screen.getByTestId("sub-heading")).toHaveTextContent(
+      "Explore the world in any direction"
+    );
   });
+
+  it("fires onClick function successfully", async () => {
+    const onClick = jest.fn();
+    render(
+      <ThemeProvider theme={lightTheme}>
+        <Page />
+      </ThemeProvider>
+    );
+
+    const button = screen.getAllByRole('button')[0];
+    fireEvent.click(button);
+
+    expect(button).toHaveTextContent("Buy Now");
+    expect(screen.getAllByRole('button')[1]).toHaveTextContent("Ways to travel");
+  });
+});
