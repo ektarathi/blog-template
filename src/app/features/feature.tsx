4793398184efebba3e07cf9d2b2d529e62556d06
Ekta Rathi <er@arenko.group>
2023-05-28 23:24:44 +0100

    Added description

--- src/app/features/feature.tsx
+++ src/app/features/feature.tsx
@@ -12,20 +12,25 @@ const Feature = () => {
         <Grid container spacing={3}>
           <Grid item xs={12} sm={4}>
             <LeftContent date-testid="left-content">
-              <Typography variant="h6" style={{paddingBottom: 20}}> Northern countries</Typography>
-                Lorem ipsum dolor amet, consecte tempor incididunt ut labore et dolore tumber tur adipisicing elit.
+              <Typography variant="h6" style={{paddingBottom: 20}} data-testid="left-section-heading"> Northern countries</Typography>
+              Northern Europe include some or all of the following areas: British Isles, Fennoscandia, 
+              the peninsula of Jutland, the Baltic plain that lies to the east, and the many islands that 
+              lie offshore from mainland Northern Europe and the main European continent.
             </LeftContent>
           </Grid>
           <Grid item xs={12} sm={4}>
             <MiddleContent date-testid="middle-content">
-              <Typography variant="h6" style={{paddingBottom: 20}}> Southern countries</Typography>
-              Lorem ipsum dolor amet, consecte tempor incididunt ut labore et dolore tumber tur adipisicing elit.
+              <Typography variant="h6" style={{paddingBottom: 20}} data-testid="middle-section-heading"> Southern countries</Typography>
+              Southern Europe is the southern region of Europe, It is also known as Mediterranean Europe, as its geography is marked by the Mediterranean Sea. 
+              It is focused on the three peninsulas located in the extreme south of the European continent.
             </MiddleContent>
           </Grid>
           <Grid item xs={12} sm={4}>
             <RightContent date-testid="right-content">
-              <Typography variant="h6" style={{paddingBottom: 20}}> Eastern countries</Typography>
-              Lorem ipsum dolor amet, consecte tempor incididunt ut labore et dolore tumber tur adipisicing elit.
+              <Typography variant="h6" style={{paddingBottom: 20}} data-testid="right-section-heading"> Eastern countries</Typography>
+              Eastern Europe is a subregion of the European continent. As a largely ambiguous term, 
+              it has a wide range of geopolitical, geographical, ethnic, cultural, and socio-economic connotations. 
+              The vast majority of the region is covered by Russia.
             </RightContent>
           </Grid>
         </Grid>
