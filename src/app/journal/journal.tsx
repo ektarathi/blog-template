1e1acb53e2a6d31823f099ba9f70af5f7f3e8e8b
Ekta Rathi <er@arenko.group>
2023-05-31 23:10:33 +0100

    Updated Journal PAge

--- src/app/journal/journal.tsx
+++ src/app/journal/journal.tsx
@@ -12,7 +12,7 @@ import AboutSection from "../components/aboutSection";
 import SupportSection from "../components/supportSection";
 import ServiceSection from "../components/servicesSection";
 
-const AboutUs = () => {
+const Journal = () => {
   return (
     <Box data-testid="about-us" mt={5} id="journal">
       <Container>
@@ -26,8 +26,52 @@ const AboutUs = () => {
             <br /> in some of the world&apos;s most breathtaking destinations.
           </SubTitle>
         </Typography>
-        <Container>
+        <Box>
           <Grid
+            container
+            spacing={{ xs: 0 }}
+            columns={{ xs: 2, sm: 12}}
+            style={{ border: "1px solid #78828c21", borderRadius: "5px", borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }}
+          >
+            <Grid item xs={4}>
+              <AboutSection />
+            </Grid>
+            <Grid item xs={4}>
+              <Image
+                src={topImage}
+                alt="Beach view"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <SupportSection />
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={{ xs: 0 }}
+            columns={{ xs: 2, sm: 12}}
+            style={{ border: "1px solid #78828c21", borderRadius: "5px", borderTopLeftRadius: 0, borderTopRightRadius: 0 }}
+          >
+            <Grid item xs={2} sm={4} md={4}>
+              <Image
+                src={leftImage}
+                alt="White mountains"
+                style={{ width: "100%", height: "100%" , borderBottomLeftRadius: "5px"}}
+              />
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <ServiceSection />
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Image
+                src={rightImage}
+                alt="lady walking in the forest"
+                style={{ width: "100%", height: "100%", borderBottomRightRadius: "5px" }}
+              />
+            </Grid>
+          </Grid>
+          {/*<Grid
             container
             spacing={3}
             style={{ border: "1px solid #78828c21", borderRadius: "5px" }}
@@ -76,21 +120,19 @@ const AboutUs = () => {
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Container>
-        <Container>
-          <InfoContent>
-            <Typography variant="subtitle1">
-              We believe that every traveler is unique, and that&apos;s why 
-              we don&apos;t believe in “one size fits all” vacations. Instead, we work closely with 
-              each client to create a personalized itinerary that is tailored to their individual 
-              preferences and vision. 
-            </Typography>
-          </InfoContent>
-        </Container>
+  </Grid>*/}
+        </Box>
+        <InfoContent>
+          <Typography variant="subtitle1">
+            We believe that every traveler is unique, and that&apos;s why we
+            don&apos;t believe in “one size fits all” vacations. Instead, we
+            work closely with each client to create a personalized itinerary
+            that is tailored to their individual preferences and vision.
+          </Typography>
+        </InfoContent>
       </Container>
     </Box>
   );
 };
 
-export default AboutUs;
+export default Journal;
